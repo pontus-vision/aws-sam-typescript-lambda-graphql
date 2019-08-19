b3ab62e1143e1899e2bcdece3890bb03a1ca396a
Peter Fajemisin <peter.fajemisin@paconsulting.com>
2019-08-19 13:06:44 +0100

    JIRA-18 Added string-format node module.

--- test/graphql/resolver/car.test.ts
+++ test/graphql/resolver/car.test.ts
@@ -1,6 +1,6 @@
-import { CreateUserRoleStack } from "../lib/CreateUserRoleStack";
-
-import { Car } from "../../../src/graphql/resolvers/car.ts";
+// import { CreateUserRoleStack } from "../lib/CreateUserRoleStack";
+//
+// import { Car } from "../../../src/graphql/resolvers/car.ts";
 
 test("2 + 3 = 5", () => {
   expect(add(2, 3)).toBe(5);
