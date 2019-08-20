c222e497dcfb7256d0500d152460d4b06cdf7c47
Peter Fajemisin <peter.fajemisin@paconsulting.com>
2019-08-20 14:44:32 +0100

    jira-20 add test for filtered queries.

--- test/car.test.ts
+++ test/car.test.ts
@@ -26,6 +26,42 @@ const allCarsNamesTestCase = {
         }
       ]
     }
+  },
+  spy: {
+    queryText: "SELECT search::jsonb FROM VEHICLES.Car",
+    queryParam: []
+  }
+};
+
+const selectedCarsNamesTestCase = {
+  id: "selected cars names",
+  query: `
+      query {
+        car(name:"Danielle") {
+           name 
+        }
+      }
+    `,
+  variables: {},
+
+  context: { sqlService: new MockSQLService() },
+
+  expected: {
+    data: {
+      car: [
+        {
+          name: "Krishna"
+        },
+        {
+          name: "Quan"
+        }
+      ]
+    }
+  },
+  spy: {
+    queryText:
+      "SELECT search::jsonb FROM VEHICLES.Car WHERE search->> 'name' = $1",
+    queryParam: ["Danielle"]
   }
 };
 
@@ -46,22 +82,30 @@ const upsertCarTestCase = {
   // expected result
   expected: {
     data: { updateCar: { status: " 200 " } }
+  },
+
+  spy: {
+    queryText:
+      "INSERT INTO VEHICLES.Car(_id, search) values ($1, $2) ON CONFLICT (_id) DO UPDATE set _id = $3, search =$4",
+    queryParam: [
+      "5",
+      '{"_id":"5","name":"Quan"}',
+      "5",
+      '{"_id":"5","name":"Quan"}'
+    ]
   }
 };
 
 describe("Test all car query cases", () => {
-  const cases = [allCarsNamesTestCase];
+  const cases = [allCarsNamesTestCase, selectedCarsNamesTestCase];
 
   cases.forEach(obj => {
-    const { id, query, variables, context, expected } = obj;
+    const { id, query, variables, context, expected, spy } = obj;
     const sqlServiceSpy = jest.spyOn(context.sqlService, "runQuery");
     test(`query: ${id}`, async () => {
       const result = await graphql(schema, query, null, context, variables);
       expect(sqlServiceSpy).toHaveBeenCalled();
-      expect(sqlServiceSpy).toHaveBeenCalledWith(
-        "SELECT search::jsonb FROM VEHICLES.Car",
-        []
-      );
+      expect(sqlServiceSpy).toHaveBeenCalledWith(spy.queryText, spy.queryParam);
 
       return expect(result).toEqual(expected);
     });
@@ -72,16 +116,13 @@ describe("Test all mutation cases", () => {
   const cases = [upsertCarTestCase];
 
   cases.forEach(obj => {
-    const { id, mutation, variables, context, expected } = obj;
+    const { id, mutation, variables, context, expected, spy } = obj;
 
     test(`Mutate: ${id}`, async () => {
       const sqlServiceSpy = jest.spyOn(context.sqlService, "runQuery");
       const result = await graphql(schema, mutation, null, context, variables);
       expect(sqlServiceSpy).toHaveBeenCalled();
-      expect(sqlServiceSpy).toHaveBeenCalledWith(
-        'INSERT INTO VEHICLES.Car(_id, search) values (\'5\', \'{"_id":"5","name":"Quan"}\') ON CONFLICT (_id) DO UPDATE set _id = \'5\', search =\'{"_id":"5","name":"Quan"}\'',
-        []
-      );
+      expect(sqlServiceSpy).toHaveBeenCalledWith(spy.queryText, spy.queryParam);
 
       return expect(result).toEqual(expected);
     });
