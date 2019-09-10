c9821a0a3f7fdced2b9c8516591e23e6335fc48a
dev <dev@pontusvision.com>
2019-09-10 11:00:26 +0100

    fixed syntax error in docker file

--- src/interfaces/types.d.ts
+++ src/interfaces/types.d.ts
@@ -1,61 +1,61 @@
 export type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
-export interface Scalars {
+export type Scalars = {
   ID: string;
   String: string;
   Boolean: boolean;
   Int: number;
   Float: number;
-}
+};
 
-export interface Car {
+export type Car = {
   __typename?: "Car";
   _id: Scalars["String"];
   name?: Maybe<Scalars["String"]>;
-}
+};
 
-export interface CarInput {
+export type CarInput = {
   _id: Scalars["String"];
   name?: Maybe<Scalars["String"]>;
-}
+};
 
-export interface Mutation {
+export type Mutation = {
   __typename?: "Mutation";
   updateCar?: Maybe<Status>;
   updateTrain?: Maybe<Status>;
-}
+};
 
-export interface MutationUpdateCarArgs {
+export type MutationUpdateCarArgs = {
   car?: Maybe<CarInput>;
-}
+};
 
-export interface MutationUpdateTrainArgs {
+export type MutationUpdateTrainArgs = {
   _id?: Maybe<Scalars["String"]>;
   name?: Maybe<Scalars["String"]>;
-}
+};
 
-export interface Query {
+export type Query = {
   __typename?: "Query";
   car?: Maybe<Array<Maybe<Car>>>;
   train?: Maybe<Array<Maybe<Train>>>;
-}
+};
 
-export interface QueryCarArgs {
+export type QueryCarArgs = {
   _id?: Maybe<Scalars["String"]>;
   name?: Maybe<Scalars["String"]>;
-}
+};
 
-export interface QueryTrainArgs {
+export type QueryTrainArgs = {
   name?: Maybe<Scalars["String"]>;
-}
+};
 
-export interface Status {
+export type Status = {
   __typename?: "Status";
   status?: Maybe<Scalars["String"]>;
-}
+};
 
-export interface Train {
+export type Train = {
   __typename?: "Train";
   _id: Scalars["String"];
   name?: Maybe<Scalars["String"]>;
-}
+};
